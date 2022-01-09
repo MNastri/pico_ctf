99b0dcb598f218ce9726077327febbccc285d0d3
MNastri <marcellonastri@gmail.com>
2022-01-09 09:35:50 -0300

    Solve Wave a flag challenge

--- 000_pico_ctf_flags.py
+++ 000_pico_ctf_flags.py
@@ -12,5 +12,10 @@ flags = {
         'flag': 'picoCTF{4p0110_1n_7h3_h0us3_67c6cc96}',
         'solution': 'inside the current path, '
                     'run "python 003_ende.py -d 003_flag.txt.en 67c6cc9667c6cc9667c6cc9667c6cc96" in a terminal'
+    },
+    'Wave a flag': {
+        'flag': 'picoCTF{b1scu1ts_4nd_gr4vy_30e77291}',
+        'solution': 'run "./004_warm -h" in a linux terminal '
+                    'after enabling execution of the file with the command "chmod +x warm"'
     }
 }
