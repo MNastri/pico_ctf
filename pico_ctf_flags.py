64b46ea54d079c350a11866a32ecf940f8e27903
MNastri <marcellonastri@gmail.com>
2022-01-09 09:22:30 -0300

    Add missing picoCTF flag for Python Wrangling challenge

--- pico_ctf_flags.py
+++ pico_ctf_flags.py
@@ -1,7 +1,16 @@
 """ The flags (answers) for the challenges. """
 flags = {
-    'Obedient Cat': {'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
-                     'solution': 'open the file in editor'},
-    'Mod 26': {'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
-               'solution': 'rot13.py'},
+    'Obedient Cat': {
+        'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
+        'solution': 'open the file in editor'
+    },
+    'Mod 26': {
+        'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
+        'solution': 'rot13.py'
+    },
+    'Python Wrangling': {
+        'flag': 'picoCTF{4p0110_1n_7h3_h0us3_67c6cc96}',
+        'solution': 'inside the current path, '
+                    'run "python 003_ende.py -d 003_flag.txt.en 67c6cc9667c6cc9667c6cc9667c6cc96" in a terminal'
+    }
 }
