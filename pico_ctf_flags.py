8f66076424b1d329a90d328bda033a91eae9dac4
MNastri <marcellonastri@gmail.com>
2022-01-09 09:05:56 -0300

    Add file with answers and solutions to the challenges

---
+++ pico_ctf_flags.py
@@ -0,0 +1,7 @@
+""" The flags (answers) for the challenges. """
+flags = {
+    'Obedient Cat': {'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
+                     'solution': 'open the file in editor'},
+    'Mod 26': {'flag': 'picoCTF{s4n1ty_v3r1f13d_2aa22101}',
+               'solution': 'rot13.py'},
+}
